204acfa32b9f896c5c2b36cb142235e8579b61e8
unknown <goodnewsdavid22@gmail.com>
2023-12-11 22:05:24 +0800

    almost done

--- gawa-master/js/main.js
+++ gawa-master/js/main.js
@@ -1,19 +1,19 @@
 const hotels = [
   {
-    name: "Firenze Apartments Lekki",
+    name: "Adun Suites Hotel",
     review: "8.1",
     rating: "4",
-    image: "images/hotelimg/hotel-img-1.webp",
-    price: "70,0000",
-    country: "Nigeria",
+    image: "images/hotelimg/Adun-suites.webp",
+    price: "12,000",
+    country: "illupeju",
   },
   {
-    name: "Titanic Ocean",
+    name: "Creme suites hotel",
     review: "8.1",
     rating: "5",
-    image: "images/hotelimg/hotel-img-2.webp",
+    image: "images/hotelimg/Creme-suites-hotel.jpg",
     price: "105,0000",
-    country: "United State Of America",
+    country: "Onipanu",
   },
   {
     name: "Radisson Blu Lagos Ikeja",
@@ -21,39 +21,39 @@ const hotels = [
     rating: "3",
     image: "images/hotelimg/RadissonBlue.jpg",
     price: "45,000",
-    country: "Dubai",
+    country: "Ikeja",
   },
   {
-    name: "Lagos Continental Hotel",
+    name: "King's celiahotel",
     review: "8.1",
     rating: "4",
-    image: "images/hotelimg/lagos-Continental.webp",
+    image: "images/hotelimg/Kings-celiahotel.jpg",
     price: "80,000",
-    country: "Greece",
+    country: "Yaba",
   },
   {
-    name: "Four Points by Sheraton Lagos ",
+    name: "Paradise Hotel",
     review: "8.1",
     rating: "5",
-    image: "images/hotelimg/fourPoint.webp",
+    image: "images/hotelimg/paradise-Hotel.jpg",
     price: "200,000",
-    country: "Scotland",
+    country: "Obanikoro",
   },
   {
-    name: "Lagos Marriott Hotel Ikeja",
+    name: "De-next hotel",
     review: "8.1",
     rating: "5",
-    image: "images/hotelimg/Lagos-Marriott-Hotel.webp",
-    price: "80,000",
-    country: "France",
+    image: "images/hotelimg/De-next-hotel.jpg",
+    price: "12,000",
+    country: "Mushin",
   },
   {
-    name: "White Emirates Hotel and Apartment",
+    name: "Peace Land Hotel",
     review: "8.1",
     rating: "3",
-    image: "images/hotelimg/White-Emirates-Hotel-and-Apartment.webp",
-    price: "55,000",
-    country: "London",
+    image: "images/hotelimg/Peace-Land-Hotel.jpg",
+    price: "15,000",
+    country: "Anthony",
   },
   {
     name: "Lagos Oriental Hotel",
@@ -61,23 +61,23 @@ const hotels = [
     rating: "4",
     image: "images/hotelimg/Lagos-Oriental-Hotel.webp",
     price: "80,000",
-    country: "Portugal",
+    country: "Oshodi",
   },
   {
-    name: "Gemstone homes and comfort",
+    name: "The Cocoon Hotel",
     review: "8.1",
     rating: "5",
-    image: "images/hotelimg/Gemstone-homes-and-comfort.webp",
+    image: "images/hotelimg/Th-Cocoon-hotel.png",
     price: "20,000",
-    country: "Italy",
+    country: "Ikeja",
   },
   {
-    name: "MITOS LUXURY SUITES (BODE THOMAS)",
+    name: "The AHI Residence",
     review: "8.1",
     rating: "4",
-    image: "images/hotelimg/mitos.webp",
+    image: "images/hotelimg/The-AHI-Residence.webp",
     price: "40,000",
-    country: "Canada",
+    country: "Shomolu",
   },
   {
     name: "Legend Hotel Lagos Airport",
@@ -85,15 +85,50 @@ const hotels = [
     rating: "2",
     image: "images/hotelimg/legend.webp",
     price: "30,000",
-    country: "Mexico",
+    country: "Bariga",
   },
   {
-    name: "Urban Height",
+    name: "Vintage Suites.jpeg",
     review: "8.1",
     rating: "4",
-    image: "images/hotelimg/urban-Height.webp",
+    image: "images/hotelimg/Vintage-Suites.jpeg",
     price: "80,0000",
-    country: "Qatar",
+    country: "Surulere",
+  },
+];
+
+const flight = [
+  {
+    name: "air line flight",
+    review: "8.1",
+    rating: "5",
+    image: "images/Coming-Soon.png",
+    price: "20,000",
+    state: "lagos",
+  },
+  {
+    name: "first flight",
+    review: "8.1",
+    rating: "2",
+    image: "images/Coming-Soon.png",
+    price: "10,000",
+    state: "Abuja",
+  },
+  {
+    name: "second flight",
+    review: "8.1",
+    rating: "3",
+    image: "images/Coming-Soon.png",
+    price: "20,000",
+    state: "ilorin",
+  },
+  {
+    name: "Thired flight",
+    review: "8.1",
+    rating: "4",
+    image: "images/Coming-Soon.png",
+    price: "70,000",
+    state: "Ibadan",
   },
 ];
 
@@ -103,14 +138,10 @@ const locations = urlParams.get("location");
 const price = urlParams.get("price");
 if (locations) {
   let filteredHotels;
-   filteredHotels = hotels.filter(
+  filteredHotels = hotels.filter(
     (list) => list.country.toLowerCase() === locations.toLowerCase()
   );
-  
-+
 
-
-  
   filteredHotels.forEach((list) => {
     $(".offers_grid").append(`<div class="offers_item rating_${list.rating}">
         <div class="row">
@@ -162,14 +193,11 @@ if (locations) {
         </div>
     </div>`);
   });
-  
-  
-}else if(price){
-  
+} else if (price) {
   const minimmumPrice = price.split("-")[0].trim();
-    const maximumPrice = price.split("-")[1].trim();
+  const maximumPrice = price.split("-")[1].trim();
   filteredHotels = hotels.filter(
-    (list) =>list.price >= minimmumPrice && list.price <= maximumPrice
+    (list) => list.price >= minimmumPrice && list.price <= maximumPrice
   );
 
   filteredHotels.forEach((list) => {
@@ -197,18 +225,7 @@ if (locations) {
                     <p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum
                         dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor.
                         Lorem ipsum dolor sit amet.</p>
-                    <div class="offers_icons">
-                        <ul class="offers_icons_list">
-                            <li class="offers_icons_item"><img src="images/post.png" alt="">
-                            </li>
-                            <li class="offers_icons_item"><img src="images/compass.png" alt="">
-                            </li>
-                            <li class="offers_icons_item"><img src="images/bicycle.png" alt="">
-                            </li>
-                            <li class="offers_icons_item"><img src="images/sailboat.png" alt="">
-                            </li>
-                        </ul>
-                    </div>
+                    
                     <div class="button book_button"><a
                             href="#">book<span></span><span></span><span></span></a></div>
                     <div class="offer_reviews">
@@ -249,18 +266,7 @@ if (locations) {
                     <p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum
                         dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor.
                         Lorem ipsum dolor sit amet.</p>
-                    <div class="offers_icons">
-                        <ul class="offers_icons_list">
-                            <li class="offers_icons_item"><img src="images/post.png" alt="">
-                            </li>
-                            <li class="offers_icons_item"><img src="images/compass.png" alt="">
-                            </li>
-                            <li class="offers_icons_item"><img src="images/bicycle.png" alt="">
-                            </li>
-                            <li class="offers_icons_item"><img src="images/sailboat.png" alt="">
-                            </li>
-                        </ul>
-                    </div>
+                    
                     <div class="button book_button"><a
                             href="#">book<span></span><span></span><span></span></a></div>
                     <div class="offer_reviews">
@@ -277,21 +283,113 @@ if (locations) {
   });
 }
 
+if (locations) {
+  let filteredFlight;
+  filteredFlight = flight.filter(
+    (list) => list.state.toLowerCase() === locations.toLowerCase()
+  );
 
-
-
-
+  filteredFlight.forEach((list) => {
+    $(".offers_grid").append(`<div class="offers_item rating_${list.rating}">
+    <div class="row">
+        <div class="col-lg-1 temp_col"></div>
+        <div class="col-lg-3 col-1680-4">
+            <div class="offers_image_container">
+                <!-- Image by https://unsplash.com/@kensuarez -->
+                <div class="offers_image_background"
+                    style="background-image:url(${list.image})"></div>
+                <div class="offer_name"><a href="single_listing.html">${list.name}</a></div>
+            </div>
+        </div>
+        <div class="col-lg-8">
+            <div class="offers_content">
+                <div class="offers_price">${list.price}<span></span></div>
+                <div class="rating_r rating_r_4 offers_rating" data-rating="4">
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                </div>
+                <p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor.
+                    Lorem ipsum dolor sit amet.</p>
+                
+                <div class="button book_button book"><a
+                        href="" >book<span></span><span></span><span></span></a></div>
+                <div class="offer_reviews">
+                    <div class="offer_reviews_content">
+                        <div class="offer_reviews_title">very good</div>
+                        <div class="offer_reviews_subtitle">10 reviews</div>
+                    </div>
+                    <div class="offer_reviews_rating text-center">8.1</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`);
+  });
+} else {
+  flight.forEach((list) => {
+    $(".offers_grid").append(`<div class="offers_item rating_${list.rating}">
+        <div class="row">
+            <div class="col-lg-1 temp_col"></div>
+            <div class="col-lg-3 col-1680-4">
+                <div class="offers_image_container">
+                    <!-- Image by https://unsplash.com/@kensuarez -->
+                    <div class="offers_image_background"
+                        style="background-image:url(${list.image})"></div>
+                    <div class="offer_name"><a href="single_listing.html">${list.name}</a></div>
+                </div>
+            </div>
+            <div class="col-lg-8">
+                <div class="offers_content">
+                    <div class="offers_price">${list.price}<span></span></div>
+                    <div class="rating_r rating_r_4 offers_rating" data-rating="4">
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                    </div>
+                    <p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum
+                        dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor.
+                        Lorem ipsum dolor sit amet.</p>
+                    <div class="button book_button"><a
+                            href="#">book<span></span><span></span><span></span></a></div>
+                    <div class="offer_reviews">
+                        <div class="offer_reviews_content">
+                            <div class="offer_reviews_title">very good</div>
+                            <div class="offer_reviews_subtitle">100 reviews</div>
+                        </div>
+                        <div class="offer_reviews_rating text-center">8.1</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`);
+  });
+}
 
 const countries = hotels.map((list) => list.country);
 countries.map((list) => {
   $(".dropdown_item_select").append(`<option>${list}</option>`);
 });
 
-$(".dropdown_item_select").on("change", () => {
-  const selectedCountry = $(".dropdown_item_select").find(":selected").val();
+$(".dropdown_item_select2").on("change", () => {
+  const selectedCountry = $(".dropdown_item_select2").find(":selected").val();
   window.location = `offers.html?location=${selectedCountry}`;
 });
 
+const flights = flight.map((list) => list.state);
+flights.map((list) => {
+  $(".dropdown_item_select2").append(`<option>${list}</option>`);
+});
+
+$(".dropdown_item_select").on("change", () => {
+  const selectedState = $(".dropdown_item_select").find(":selected").val();
+  window.location = `offers.html?location=${selectedState}`;
+});
 
 $(".pricing-tag").append(`
   <option>20,000 - 50,000</option>
@@ -302,9 +400,4 @@ $(".pricing-tag").append(`
 $(".pricing-tag").on("change", () => {
   const selectedPrice = $(".pricing-tag").find(":selected").val();
   window.location = `offers.html?price=${selectedPrice}`;
-
 });
-
-
-
-
